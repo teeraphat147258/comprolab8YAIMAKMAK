6d722c0afe8f307640234621149cafdc606c5e1f
teeraphat147258 <teeraphat147258@gmail.com>
2021-12-03 11:12:15 +0700

    lab8_2 completed

--- lab8_2.cpp
+++ lab8_2.cpp
@@ -16,3 +16,36 @@ Fahsai: Next Monday....that is OK!!! I'm looking forward to watching Star Wars V
 Luke Skywalker: May the Force be with you krub
 Fahsai: 555+ see you Next Monday. Bye Bye \(^ ^)/
 */
+#include <iostream>
+#include <cmath>
+#include <string>
+using namespace std;
+
+int main(){
+string name;
+int x;
+string movie;
+string day;
+string sentence;
+cout << "Fahsai: Sawadee ka...Can you tell me your name?" << "\n";
+cout << "?????: ";
+getline(cin,name);
+cout <<  "Fahsai: Wow!!! " << name << " is a really cool name." << "\n";
+cout << "Fahsai: I think you are an Engineering student. What is your student ID?" << "\n";
+cout << name << ": ";
+cin >> x;
+cout << "Fahsai: I think you may be GEAR " <<  (x/10000000)-12 << ". I have a free movie ticket for you."<< "\n";
+cout << "Fahsai: Let's go to the cinema together!!!"<< "\n";
+cout << "Fahsai: What movie do you want to watch?" << "\n";
+cin.ignore();
+cout << name << ": ";
+getline(cin,movie);
+cout << "Fahsai: So....which day are you free to go with me?" << "\n";
+cout << name << ": ";
+getline(cin,day);
+cout << "Fahsai: " << day << "....that is OK!!! I'm looking forward to watching " << movie << " with you." << "\n";
+cout << name << ": ";
+getline(cin,sentence); 
+cout << "Fahsai: 555+ see you " << day << ". Bye Bye \\(^ ^)/";
+return 0;
+}
